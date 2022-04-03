e38d72f8c023cfea67c8548731f238bbbb228204
Miroslav I <styylzeh@gmail.com>
2022-04-03 19:58:07 +0300

    feat: fixed theme and wrapped app with theme provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { Container, CssBaseline } from '@mui/material';
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 import Header from './components/layout/navigation/Header/Header';
 import HomePage from './views/Home/HomePage/HomePage';
 import { getWindowWidth } from './utils/helpers/window-helper';
+import theme from './styles/theme';
 
 const App = () => {
   const screenWidth = getWindowWidth();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       {screenWidth > 900 ? (
@@ -19,7 +21,7 @@ const App = () => {
           <HomePage />
         </Container>
       )}
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ client/src/styles/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'SF Pro Display',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
+export default theme;
