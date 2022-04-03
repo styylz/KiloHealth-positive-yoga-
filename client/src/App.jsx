97d93995dd8d52d7d0a8b918c0e73420800f67bf
Miroslav I <styylzeh@gmail.com>
2022-04-03 16:01:03 +0300

    feat: added css reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import Header from './components/layout/navigation/Header/Header';
 import HomePage from './views/Home/HomePage/HomePage';
 
 const App = () => (
   <>
+    <CssBaseline />
     <Header />
     <HomePage />
   </>
