faefc572fe09c2d0cc121e7833c2b51d75e8f5ad
Miroslav I <styylzeh@gmail.com>
2022-04-03 18:13:06 +0300

    feat: created benefitsData

---
+++ client/src/data/benefitsData.js
@@ -0,0 +1,28 @@
+const benefitsData = [
+  {
+    id: '1',
+    title: 'Each program adapts to your age or fitness level',
+  },
+  {
+    id: '2',
+    title: 'Mindful way to exercise and get real results',
+  },
+  {
+    id: '3',
+    title: 'Effective and long-term lasting results',
+  },
+  {
+    id: '4',
+    title: 'Suited activities that benefit both the mind and body',
+  },
+  {
+    id: '5',
+    title: 'Low-intensity but highly-effective workouts',
+  },
+  {
+    id: '6',
+    title: 'Extra attention to muscle, joint and back health',
+  },
+];
+
+export default benefitsData;
